443c4c1aa8cad343d38aed7779c9f9a3ec0d7a19
amanmeka <aman.mekas@gmail.com>
2026-01-08 12:53:06 -0600

    first

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/* * * * * * * * * * * * * * * * * * * * *
+//Smoke Test - provided to give you a quick check of your stack code
+#include "Stack.h"
+#include <iostream>
+using namespace std;
+int main() {
+    Stack myStack;
+    myStack.push(10);
+    myStack.push(20);
+    myStack.push(30);
+    cout << "Top element: " << myStack.peek() << endl; // Outputs 30
+    myStack.pop();
+    myStack.display(0); // Displays: 20 10
+    return 0;
+}
+//End Smoke Test
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//* * * * * * * * * * * * * * * * * * * *
+
+// <<<< YOUR solveTowersOfHanoi(n) GOES HERE >>>>
+
+
+// * * * * * * * * * * * * * * * * * * * * * * *
+// Use this main.
+int main() {
+  int n; // Number of disks
+  cout << "Enter the number of disks: ";
+  cin >> n;
+  if (n <= 0) {
+    cout << "Number of disks must be greater than 0." << endl;
+    return 1;
+  }
+  cout << "Steps to solve the Towers of Hanoi:" << endl;
+  solveTowersOfHanoi(n);
+  return 0;
+}
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
